c17830c037a07fa6e694544a507cc06cfd72f8de
Edmund Ciego <edmundciego@users.noreply.github.com>
2025-11-24 20:47:51 -0600

    Add Firebase configuration to the service worker for messaging functionality

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,13 +6,14 @@ importScripts(
 );
 // // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyCNBXZ4uEOU7hAHkvEZJGIsUw3WN3hC38M",
+  authDomain: "streetsideco.firebaseapp.com",
+  databaseURL: "https://streetsideco.firebaseio.com",
+  projectId: "streetsideco",
+  storageBucket: "streetsideco.appspot.com",
+  messagingSenderId: "296214775948",
+  appId: "1:296214775948:web:28245f40ca12c7043dd3a9",
+  measurementId: "G-725TBL6QQ2",
 };
 
 firebase?.initializeApp(firebaseConfig);
